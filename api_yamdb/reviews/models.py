8fe7f301f6e80ba842a80dccb6ee5a68cf2559b9
Shamil Kutdusov <Shombai11@yandex.ru>
2023-06-29 01:42:59 +0300

    Add validators к reviews

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from .validators import max_length_validator
+
+
+class Reviews(models.Model):
+    """Отзывы."""
+    pass
+
+
+class Comment(models.Model):
+    """Коментарий."""
+    pass
