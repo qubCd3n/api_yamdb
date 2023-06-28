8e295b3a19f4cb86738b1bdc8ae57a7f548ce954
Даниил Авраменко <qubcd3n@gmail.com>
2023-06-28 23:11:58 +0300

    Добавленно приложение users, описана его модель, добавлены урлы api и изменение в сеттингах

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,5 @@
+import os
 from pathlib import Path
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,9 +21,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
-    'reviews',
+    'rest_framework',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
+    'users.apps.UsersConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -103,3 +106,25 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+}
+
+AUTH_USER_MODEL = 'users.User'
+
+USER = 'user'
+
+MODERATOR = 'moderator'
+
+ADMIN = 'admin'
+
+ROLES = (
+    (USER, 'user'),
+    (MODERATOR, 'moderator'),
+    (ADMIN, 'admin'),
+)
---
+++ api_yamdb/users/models.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from api_yamdb.settings import ROLES, USER, ADMIN, MODERATOR
+from django.core.validators import RegexValidator
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=100,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[\w.@+-]+$'
+            )
+        ],
+    )
+
+    email = models.EmailField(
+        verbose_name='Почта',
+        max_length=254,
+        unique=True,
+    )
+
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+
+    bio = models.TextField(
+        verbose_name='Биография',
+        blank=True,
+        null=True,
+    )
+
+    role = models.CharField(
+        verbose_name='Полномочия',
+        choices=ROLES,
+        default=USER,
+        max_length=10,
+    )
+
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN or self.is_staff
+
+    def __str__(self) -> str:
+        return f'{self.username}'
