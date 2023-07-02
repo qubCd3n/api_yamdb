7bc9c0012604b6ac05e4c6fb75acba3138fa0dcc
Shamil Kutdusov <Shombai11@yandex.ru>
2023-07-02 19:31:14 +0300

    add urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,14 +4,22 @@ from rest_framework.routers import DefaultRouter
 from .views import (CategoryViewSet, GenreViewSet, TitleViewSet, ReviewsViewSet, CommentViewSet
                     TokenReceiveViewSet, UserRegistrationViewSet, UserViewSet)
 
-router = DefaultRouter()
+router_v1 = DefaultRouter()
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewsViewSet, basename='review'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
 
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('genres', GenreViewSet, basename= 'genres')
 router.register('titles', TitleViewSet, basename='titles')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
     # path('auth/token/', TokenReceiveViewSet.as_view({'post': 'create'}),),
     # path('auth/signup/',UserRegistrationViewSet.as_view({'post': 'create'}),),
 ]
